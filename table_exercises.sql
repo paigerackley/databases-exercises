5069635f109709a7f85fd97417f10ff3d01d5175
Paige Rackley <paige.e.rackley@gmail.com>
2022-03-30 15:12:26 -0500

    Table Exercises

---
+++ table_exercises.sql
@@ -0,0 +1,10 @@
+use employees;
+
+SHOW tables;
+
+-- 6. Which table do you think contains a numeric type column? Salaries, dept_emp
+-- 7. Which table do you think contains a string? I feel like most could have strings besides dept_emp, because I don't know if that contains number(s) or letters.
+-- 8. Which table do you think contains a date? Possibly dept_emp, the title is vague so no telling what it means.
+-- 9. What is the relationship between the employees and departments tables? Both are in the employees database.
+
+SHOW CREATE TABLE dept_manager;
