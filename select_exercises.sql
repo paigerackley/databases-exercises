7a402932f11ff1f6cf4f65eeb96566577aab6bea
Paige Rackley <paige.e.rackley@gmail.com>
2022-03-31 10:01:24 -0500

    Finished exercises

---
+++ select_exercises.sql
@@ -0,0 +1,21 @@
+USE albums_db;
+
+-- 3A. How many rows are in the albums table? 31
+-- 3B. How many unique artist names are in the albums table? 23
+-- 3C. What is the primary key for the albums table? id
+-- 3D. What is the oldest release date for any album in the albums table? What is the most recent release date? The Beatles Sgt Peppers in 1967; Adele's 21 in 2011.
+-- 4A. The name of all albums by Pink Floyd: The Dark Side of The Moon and The Wall.
+-- 4B. The year Sgt. Pepper's was released: 1967.
+-- 4C. The genre for the album Nevermind: Grunge, Alternative Rock.
+-- 4D. Which albums were released in the 1990's: The Immaculate Collection by Madonna, Dangerous by Michael Jackson, 
+-- Metallica by Metallica, Nevermind by Nirvana, The Bodyguard soundtrack, Jagged Little Pill by Alanis Morisette, 
+-- Falling Into You by Celine Dion, Come On Over By Shania Twain, Let's Talk About Love by Celine Dion, Titanic soundtrack, Supernatural by Santana
+-- 4E. Which albums had less than 20 million certified sales: Born in the USA by Bruce Springsteen, The Immaculate Collection by Madonna,
+-- Bad by Michael Jackson, Let's Talk About Love by Celine Dion, Titanic soundtrack, Dirty Dancing soundtrack, Brothers In Arms by Dire Straits,
+-- The Wall by Pink Floyd, Nevermind by Nirvana, Dangerous by Michael Jackson, Abbey Road by The Beatles, Grease soundtrack,
+-- Sgt Peppers by The Beatles.
+-- 4F. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"? 
+-- The Beatles Sgt. Pepper, 1, and Abbey Road, Born in the USA by Bruce Springsteen, Supernatural by Santana.
+-- I was able to pull up all results and could see where Rock was a genre as well as others or there were more specific types of rock. As to why these aren't included, I don't know. 
+
+
